5e86e724af7a7118b8b607c244a8c42e496d6e80
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-29 13:04:16 -0500

    funciona registrar

--- assets/js/registra.js
+++ assets/js/registra.js
@@ -1,5 +1,32 @@
+var miCoder=new Coder();
+function Coder(){
+  this.arrayCoder=[];
+  this.contador=0;
+  this.agrega=function(nombre,apellido,correo,contraseña){
+    this.arrayCoder.push({
+       id:this.contador,
+       nombre:nombre,
+       apellido:apellido,
+       correo:correo,
+       contraseña:contraseña
+    })
+    this.contador ++;
+  }
+}
+
+var nombre=document.getElementById("nombre");
+var apellido=document.getElementById("apellido");
+var correo=document.getElementById("correo");
+var contraseña=document.getElementById("contraseña");
+
+
 document.getElementById("Registro").addEventListener("click", function(e){
   e.preventDefault();
+miCoder.agrega(nombre.value , apellido.value , correo.value , contraseña.value);
+
+  localStorage.setItem("Laboratoria", JSON.stringify(miCoder));
+  console.log(miCoder.arrayCoder.length);
   window.location="bienvenida.html";
-  
+
+
 });
