13554a432a57c899c2cfb0c13adc83d78404931e
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-29 13:58:33 -0500

    completando

--- assets/js/editar.js
+++ assets/js/editar.js
@@ -1,4 +1,21 @@
-document.getElementById("Guardar").addEventListener("click",function(e){
-    e.preventDefault();
-    window.location="bienvenida.html"
+var codeadora = JSON.parse(localStorage.getItem("Laboratoria"));
+	var inputsEditar = document.getElementsByTagName("input");
+	inputsEditar[0].value = codeadora.nombre;
+	inputsEditar[1].value = codeadora.apellido;
+	inputsEditar[2].value = codeadora.correo;
+	inputsEditar[3].value = codeadora.contraseña;
+
+	document.getElementById("Guardar").addEventListener("click",function(){
+		if(inputsEditar[0].length!=0 && inputsEditar[1].length!=0 && inputsEditar[2].length!=0 && inputsEditar[3].length!=0){
+			codeadora.nombre=inputsEditar[0].value;
+			codeadora.apellido= inputsEditar[1].value
+			codeadora.edad = inputsEditar[2].value;
+			codeadora.genero = inputsEditar[3].value;
+			localStorage.setItem("Laboratoria",JSON.stringify(codeadora));
+			window.location="bienvenida.html"
+		}
+		else{
+			alert("Todos los campos son obligatorios");
+
+}
 });
