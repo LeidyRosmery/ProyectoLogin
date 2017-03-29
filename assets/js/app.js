57eff2d3ce12f6085c8e8736658dfb337fcbc6a3
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-29 11:11:47 -0500

    inicio

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+//console.log("ingresa");
+
+document.getElementById("registrar").addEventListener("click",function(e){
+  e.preventDefault();
+window.location="registrar.html";
+});
+
+document.getElementById("iniciaSesion").addEventListener("click", function(e){
+  e.preventDefault();
+window.location="inicioSesion.html";
+});
